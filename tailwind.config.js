2ba5e4028af46f589af34a0210130cf4c70f78c1
Devanshu Jangid <devanshujangid1234@gmail.com>
2025-09-20 12:55:08 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        warmWhite: '#FDF6EC',
+        softBlue: '#A9CFE7',
+      },
+      boxShadow: {
+        // Custom glow effect
+        'glow': '0 0 15px rgba(0, 255, 255, 0.7), 0 0 30px rgba(0, 255, 255, 0.7), 0 0 60px rgba(0, 255, 255, 0.7)',
+        'glow-hover': '0 0 25px rgba(0, 255, 255, 1), 0 0 50px rgba(0, 255, 255, 1), 0 0 100px rgba(0, 255, 255, 1)',
+      }
+    },
+  },
+  plugins: [],
+}
